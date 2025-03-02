3ce2c1fe1cbaa84311eaeee9c6ee003a4bfe6c51
Ben@Omnitrix <benfranlkin1990@gmail.com>
2025-03-02 16:41:45 +0530

    added object.js file

---
+++ object.js
@@ -0,0 +1,9 @@
+let obj = {
+    name: "sagar",
+    occupation: "software developer",
+    designation: "backend developer",
+    age: "24",
+    phone: "6398141162"
+};
+
+console.log("My details: ",obj);
